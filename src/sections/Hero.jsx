e10deec5912e6d2e337377b57d94feed02fc12a9
Garv Agarwal <garv.agarwal2409@gmail.com>
2026-06-19 16:24:49 +0530

    updated hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Button from "../components/Button";
-import AnimatedBorderButton from "../components/animatedBorderButton";
+import AnimatedBorderButton from "../components/AnimatedBorderButton";
 import {
   ArrowRight,
   Download,
